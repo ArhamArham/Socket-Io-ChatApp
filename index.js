475bff8b9378038afc0320031498568eb966aadf
ArhamArham <arhamroshan443@gmail.com>
2020-06-03 15:23:52 +0500

    second release

---
+++ index.js
@@ -0,0 +1,39 @@
+let app = require('express')()
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+});
+
+http.listen(3000, ()=>{
+    console.log('Connected');
+    
+})
+io.on('connection',(socket)=>{
+    io.emit('connections',Object.keys(io.sockets.connected).length);
+    
+    
+    socket.on('disconnect',()=>{
+        console.log('Disconnected');
+        
+    })
+    socket.on('Created',(data)=>{
+        socket.broadcast.emit('Created',(data))
+    })
+    socket.on('chat-message',(data)=>{
+        socket.broadcast.emit('chat-message',(data))
+    })
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',(data))
+    })
+    socket.on('StopTyping',(data)=>{
+        socket.broadcast.emit('StopTyping',(data))
+    })
+    socket.on('joined',(data)=>{
+        socket.broadcast.emit('joined',(data))
+    })
+    socket.on('leaved',(data)=>{
+        socket.broadcast.emit('leaved',(data))
+    })
+})
